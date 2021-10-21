56bec4d3eec375a60b05d79809901b261d746252
duyankim <arbolkim@gmail.com>
2021-10-21 22:36:24 +0900

    [test] $5 * 2 = $10 test case 작성

---
+++ test/multiply/MutiplyTest.java
@@ -0,0 +1,9 @@
+package multiply;
+
+public class MultiplyTest {
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amout);
+    }
+}
