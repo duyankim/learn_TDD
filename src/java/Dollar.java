8477c745bf4f3d25930ab2ae3eed385f96a18eea
duyankim <arbolkim@gmail.com>
2021-10-24 01:41:58 +0900

    [refactor] Dollar 클래스 부작용 제거

--- src/java/Dollar.java
+++ src/java/Dollar.java
@@ -1,13 +1,13 @@
 package java;
 
 public class Dollar {
-    int amount;
+    public int amount;
 
-    Dollar(int amount) {
+    public Dollar(int amount) {
         this.amount = amount;
     }
 
-    public void times(int multiplier) {
-        amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
---
+++ test/multiply/MultiplyTest.java
@@ -0,0 +1,19 @@
+package multiply;
+
+import org.junit.Test;
+
+import java.Dollar;
+
+import static org.junit.Assert.assertEquals;
+
+class MultiplyTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
+    }
+}
